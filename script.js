91468fe4e8df73d13c4275f96b09c378fe4cf096
seanlee1379@gmail.com <seanlee1379@gmial.com>
2022-03-08 09:38:35 -0800

    JS edits

--- script.js
+++ script.js
@@ -16,10 +16,12 @@ function displayQuestion(count) {
         var answer1 = document.getElementById("answer1");
         var wrong1 = document.getElementById("wrong1");
         var wrong2 = document.getElementById("wrong2");
-        question1.textContent = "What does API stand for?";
+        
         var answerBtn = document.createElement("button");
         var wrong1Btn = document.createElement("button");
         var wrong2Btn = document.createElement("button");
+
+        question1.textContent = "What does API stand for?";
         answerBtn.setAttribute("data-answer","Application Programming Interface");
         wrong1Btn.setAttribute("data-wrong1","Advanced Python Initiative");
         wrong2Btn.setAttribute("data-wrong2","Automatic Processing Identification");
@@ -28,7 +30,6 @@ function displayQuestion(count) {
         wrong2Btn.textContent = "Automatic Processing Identification";
 
         answer1.appendChild(answerBtn)
-        console.log(answerBtn)
         wrong1.appendChild(wrong1Btn)
         wrong2.appendChild(wrong2Btn)
 
@@ -50,10 +51,17 @@ function displayQuestion(count) {
         wrong1Btn.textContent = "CSS";
         wrong2Btn.textContent = "HTML";
 
+        console.log(wrong1.children[0].wrong1)
+
+        
+
+        wrong1.children[0].removeChild(wrong1)
+       
+
         answer1.appendChild(answerBtn)
-        console.log(answerBtn)
         wrong1.appendChild(wrong1Btn)
         wrong2.appendChild(wrong2Btn)
+
     } else if (count === 3) {
         var question1 = document.getElementById("question1");
         var answer1 = document.getElementById("answer1");
@@ -64,6 +72,10 @@ function displayQuestion(count) {
         var wrong1Btn = document.createElement("button");
         var wrong2Btn = document.createElement("button");
 
+        answer1.removeChild(answerBtn)
+        wrong1.removeChild(wrong1Btn)
+        wrong2.removeChild(wrong2Btn)
+
         question1.textContent = "Who created the first computer?";
         answerBtn.setAttribute("data-answer","Charles Babbage");
         wrong1Btn.setAttribute("data-wrong1","Alan Turing");
@@ -73,9 +85,9 @@ function displayQuestion(count) {
         wrong2Btn.textContent = "Per Mertesacker";
             
         answer1.appendChild(answerBtn)
-        console.log(answerBtn)
         wrong1.appendChild(wrong1Btn)
         wrong2.appendChild(wrong2Btn)
+
     } else if (count === 4) {
         var question1 = document.getElementById("question1");
         var answer1 = document.getElementById("answer1");
@@ -85,6 +97,10 @@ function displayQuestion(count) {
         var answerBtn = document.createElement("button");
         var wrong1Btn = document.createElement("button");
         var wrong2Btn = document.createElement("button");
+
+        answer1.removeChild(answerBtn)
+        wrong1.removeChild(wrong1Btn)
+        wrong2.removeChild(wrong2Btn)
         
         question1.textContent = "What does DOM stand for?";
         answerBtn.setAttribute("data-answer","Designated Orientation Marker");
@@ -95,9 +111,9 @@ function displayQuestion(count) {
         wrong2Btn.textContent = "Document Object Model";
 
         answer1.appendChild(answerBtn)
-        console.log(answerBtn)
         wrong1.appendChild(wrong1Btn)
         wrong2.appendChild(wrong2Btn)
+
     } else if (count === 5) {
         var question1 = document.getElementById("question1");
         var answer1 = document.getElementById("answer1");
@@ -117,7 +133,6 @@ function displayQuestion(count) {
         wrong2Btn.textContent = "hashtag";
 
         answer1.appendChild(answerBtn)
-        console.log(answerBtn)
         wrong1.appendChild(wrong1Btn)
         wrong2.appendChild(wrong2Btn)
     } 
@@ -126,32 +141,15 @@ var questionCount = 1;
 var answers = ["Application Program Interface","CSS","Charles Babbage","Document Object Model","period"]
 displayQuestion(questionCount)
 
-// var question2 = document.getElementById("question2");
-// var question3 = document.getElementById("question3");
-// var question4 = document.getElementById("quesiton4");
-// var quesiton5 = document.getElementById("question5");
-
-// var answer2 = document.getElementById("answer2")
-// var answer3 = document.getElementById("answer3")
-// var answer4 = document.getElementById("answer4")
-// var answer5 = document.getElementById("answer5")
-// var wrong2 = document.getElementById("wrong2")
-// var wrong22 = document.getElementById("wrong22")
-// var wrong3 = document.getElementById("wrong3")
-// var wrong33 = document.getElementById("wrong33")
-// var wrong4 = document.getElementById("wrong4")
-// var wrong44 = document.getElementById("wrong44")
-// var wrong5 = document.getElementById("wrong5")
-// var wrong55 = document.getElementById("wrong55")
-
-
-
 answer1.addEventListener("click",function(event){
     if (event.target.matches("button")) {
         var userGuess = event.target.getAttribute("data-answer");
         console.log(userGuess);
-        
+        function correct() {
+
+        }
         questionCount++;
         displayQuestion(questionCount)
     } 
 })
+
